d536427fc523f0b64ce19e2913374cf567c99ad0
omari <omari.jalagania@gmail.com>
2021-10-31 01:41:20 +0400

    filter with dates

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,12 +2,20 @@ import React from "react";
 import classes from "./button.module.css";
 import Link from "next/link";
 
-function button(props) {
+function Button(props) {
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        <a className={classes.btn}> {props.children} </a>
+      </Link>
+    );
+  }
+
   return (
-    <Link href={props.link}>
-      <a className={classes.btn}> {props.children} </a>
-    </Link>
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
   );
 }
 
-export default button;
+export default Button;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,22 @@
 import React from "react";
-
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { useRouter } from "next/router";
 function Events() {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  const findEventHandler = (year, month) => {
+    const fullpath = `/events/${year}/${month}`;
+    router.push(fullpath);
+  };
+
   return (
-    <div>
-      <h1>Events</h1>
-    </div>
+    <>
+      <EventsSearch onSearch={findEventHandler} />
+      <EventList items={events} />
+    </>
   );
 }
 
